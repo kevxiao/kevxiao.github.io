4b72dcee4544de59495dbc3895b3c78939834c44
Kevin Xiao <kevinxiao2@gmail.com>
2016-01-19 00:33:48 -0500

    Added content to site and fixed formatting

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,18 @@
-function equalizeHeight() {
-    var e = $("html").not(".ie6").find(".section");
+function sizeBanner() {
+    var e = $("html").not(".ie6").find("#banner");
     e.css({
         "min-height": $(window).height() - 30 + "px"
-    }), $(window).resize(function() {
+    });
+
+    $(window).resize(function() {
         e.css({
             "min-height": $(window).height() - 30 + "px"
         })
     })
+
+    $(".intro").css({
+        "top": ($(".intro").parent().parent().height()/2 - $(".intro").height()/2).toString() + "px"
+    });
 }
 
 function onScroll() {
@@ -17,46 +23,70 @@ function onScroll() {
         i.position().top <= e && i.position().top + i.height() > e ? ($("#menu li a").removeClass("active"), o.addClass("active")) : o.removeClass("active")
     })
 }
+
 var num = 50;
+
 $(window).bind("scroll", function() {
     jQuery(window).scrollTop() > num ? jQuery(".main-menu").addClass("floating-menu") : jQuery(".main-menu").removeClass("floating-menu")
-}), equalizeHeight(), (new WOW).init(), smoothScroll.init({
+});
+
+sizeBanner();
+
+(new WOW).init();
+
+smoothScroll.init({
     speed: 1e3,
     easing: "easeInOutCubic",
     offset: 0,
     updateURL: !1,
     callbackBefore: function() {},
     callbackAfter: function() {}
-}), $(document).on("scroll", onScroll), $("div.bgParallax").each(function() {
+});
+
+$(document).on("scroll", onScroll);
+
+$("div.bgParallax").each(function() {
     var e = $(this);
     $(window).scroll(function() {
         var o = -($(window).scrollTop() / e.data("speed")),
             i = "50% " + o + "px";
         e.css("background-position", i)
     })
-}), $(window).load(function() {
+});
+
+$(window).load(function() {
     $(".hcaption").hcaptions({
         effect: "fade"
     })
-}), $(document).ready(function() {
+});
+
+$(document).ready(function() {
     $("a.nivo-light").nivoLightbox({
         effect: "fade",
         theme: "default",
         keyboardNav: !0
     })
-}), $(".carousel").carousel({
+});
+
+$(".carousel").carousel({
     interval: 3e3
-}), jQuery(document).ready(function() {
+});
+
+jQuery(document).ready(function() {
     var e = 220,
         o = 500;
     jQuery(window).scroll(function() {
         jQuery(this).scrollTop() > e ? jQuery(".back-to-top").fadeIn(o) : jQuery(".back-to-top").fadeOut(o)
-    }), jQuery(".back-to-top").click(function(e) {
+    });
+
+    jQuery(".back-to-top").click(function(e) {
         return e.preventDefault(), jQuery("html, body").animate({
             scrollTop: 0
-        }, o), !1
+        }, o), !1;
     })
-}), $("#contact-form").bootstrapValidator({
+});
+
+$("#contact-form").bootstrapValidator({
     message: "This value is not valid",
     feedbackIcons: {
         valid: "glyphicon glyphicon-ok",
@@ -89,8 +119,14 @@ $(window).bind("scroll", function() {
             }
         }
     }
-}), $(window).load(function() {
-    $("#status").fadeOut(), $("#preloader").delay(350).fadeOut("slow"), $("body").delay(350).css({
+});
+
+$(window).load(function() {
+    $("#status").fadeOut();
+
+    $("#preloader").delay(350).fadeOut("slow");
+
+    $("body").delay(350).css({
         overflow: "visible"
     })
 });
